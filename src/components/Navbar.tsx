0d34e3d12404d5dadb59d70d316e0be2791b7ac9
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2025-01-30 13:29:08 +0000

    Implement authentication middleware and sign-in form with validation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,6 @@ import { buttonVariants } from "./ui/button";
 import Link from "next/link";
 import { LogOut } from "lucide-react";
 
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-
 interface RouteProps {
 	href: string;
 	label: string;
@@ -32,8 +30,7 @@ const routeList: RouteProps[] = [
 ];
 
 export async function Navbar() {
-	const isSubscribed = true;
-	const { isAuthenticated } = useKindeBrowserClient();
+
 	return (
 		<header
 			className="sticky border-b-[1px] top-0 z-40 w-full  dark:border-b-slate-700 overflow-x-hidden
